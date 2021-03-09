44e5c3be3dd9a2cbc02046212db60c48ef5fd9a0
Franco Calvino <frcalvino@est.frba.utn.edu.ar>
2021-03-09 08:15:39 -0300

    Actualizacion

--- js/index/BarFunctions.js
+++ js/index/BarFunctions.js
@@ -3,12 +3,16 @@ window.onload = () => {
     var item, smallImgs = document.querySelectorAll("#product-bar img");
     var bigImg = document.getElementById("big-img"), circle = document.getElementById("half-circle");
     var h1 = document.querySelector("#wrapper-boton h1"), a = document.querySelector("#boton a");
+
+    addEvent(a, "click", () => {
+        sessionStorage.setItem("ImgList", "barbijos"); //Inicializo con el boton para barbijos
+    });
     
     function ChangeProduct (event)
     {
         bigImg.src = this.src;//event.target.src;
         addEvent(a, "click", () => {
-            sessionStorage.setItem('ImgList', event.target.id);
+            sessionStorage.setItem("ImgList", event.target.id);
         });
 
         if (this.id === "barbijos")
